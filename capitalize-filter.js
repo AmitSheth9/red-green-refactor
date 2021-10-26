7bbf0428cdf0486a39088c224bfc97f25a97a9bc
AmitSheth9 <amit.sheth9@gmail.com>
2021-10-26 10:51:23 -0700

    test passed using for loop

--- capitalize-filter.js
+++ capitalize-filter.js
@@ -1,5 +1,15 @@
-const capAndFilter = () => {
+//loop through array of strings
+//for each string not starting with F/f capitalize
+//
 
+const capAndFilter = (arr) => {
+    const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i].charAt(0) !== 'f') && (arr[i].charAt(0) !== 'F')) {
+      newArr.push(arr[i].toUpperCase());
+    }
+  }
+  return newArr;
 };
 
 module.exports = capAndFilter;
--- capitalize-filter.test.js
+++ capitalize-filter.test.js
@@ -2,8 +2,8 @@ const capFilter = require('./capitalize-filter');
 
 describe('capFilter', () => {
   it('capitalize all strings and filters out strings that start with F/f', () => {
-    const arr = ['test', 'fire', 'earth', 'sun', 'filter'];
-    expect(capFilter(arr)).toEqual(['TEST', 'fire', 'EARTH', 'SUN', 'FILTER'
+    const arr = ['test', 'fire', 'earth', 'sun', 'Filter'];
+    expect(capFilter(arr)).toEqual(['TEST', 'EARTH', 'SUN'
 
     ]);
   });
